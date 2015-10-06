ebf01e46df8e10cd2aec1e696503ba39a88ab23f
Alex Schokking <alexs@alexs.local>
2015-10-06 09:32:50 -0700

    up version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except IOError:
 
 setup(
     name='django-contrib-comments',
-    version='1.6.1',
+    version='1.6.2',
     url="http://github.com/django/django-contrib-comments",
     description='The code formerly known as django.contrib.comments.',
     long_description=long_description,
